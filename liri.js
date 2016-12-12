d72a975eb79c9a216bbbcb075e0b7e6b1d2aad35
Richard Dotchin <richard.dotchin@gmail.com>
2016-12-12 17:10:09 -0500

    spotify added to liri.js

--- liri.js
+++ liri.js
@@ -3,6 +3,8 @@
 var twitter = require('twitter');
 //requires the request npm package 
 var request = require('request');
+//require the spotify npm package
+var spotify = require('spotify');
 //link the keys.js file to liri
 var twitterKeysFile = require('./keys.js');
 
@@ -15,16 +17,18 @@ var client = new twitter(twitterKeys);
 var action = process.argv[2];
 //function that pulls recent tweets when action is set to my-tweets
 function twitterFunc() {	
-var userName = process.argv[3] || 'RichardDotchin';
+	var userName = process.argv[3] || 'RichardDotchin';
+	var params = {screen_name: userName};
 
-var params = {screen_name: userName};
-client.get('statuses/user_timeline', params, function(error, tweets, response){
-	if (error) return console.log(error);
-	console.log("Twitter user: @" + params.screen_name + '\n')
-	for(var i = 0; i < tweets.length; i++){
-		console.log('Date: ' + tweets[i].created_at + '\nTweeted: ' + tweets[i].text + '\n');
-	}
-	})
+	client.get('statuses/user_timeline', params, function(error, tweets, response){
+		if (error) return console.log(error);
+		console.log("Twitter user: @" + params.screen_name + '\n')
+		for(var i = 0; i < tweets.length; i++){
+			console.log('Date: ' + tweets[i].created_at + '\nTweeted: ' + tweets[i].text + '\n');
+			//setting so only a maximum of 20 tweets are shown
+			if(i == 20) {break}
+		}
+	});
 }
 
 //function to be called when the user sets the variable action to movie-this
@@ -52,15 +56,33 @@ function omdbFunc(){
 	    			'Rotten Tomatoes Rating: ' + body.tomatoUserMeter + '\n' +
 	    			'Rotten Tomatoes URL: ' + body.tomatoURL);
 	  }
-	})
+	});
+}
+
+function spotifyFunc(){
+	var song = process.argv[3] || 'never gonna give you up'; 
+
+	spotify.search({type: 'track', query: song}, function(err, data){
+		if(err) {console.log(err)}
+			
+			/*console.log(data.tracks.items[0]);*/
+			//console.log info for the song searched to the terminal
+			console.log('Artist: ' + data.tracks.items[0].artists[0].name + '\n' +
+						'Song: ' + data.tracks.items[0].name + '\n' +
+						'Spotify URL: ' + data.tracks.items[0].external_urls.spotify);
+	});
+
 }
 
 //switch statement calling the function related to the users action request
 switch(action){
-	case 'my-tweets':
+	case 'tweets':
 	twitterFunc();
 	break
-	case 'movie-this':
+	case 'movie':
 	omdbFunc();
+	break
+	case 'spotify':
+	spotifyFunc();
 	break
 }
