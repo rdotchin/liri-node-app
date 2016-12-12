4083213a65d48b6f6a192002b39fa6f9b744a4ff
Richard Dotchin <richard.dotchin@gmail.com>
2016-12-12 14:59:53 -0500

    moved omdb function to liri.js file

--- liri.js
+++ liri.js
@@ -1,29 +1,66 @@
 //6. write the code you need to grab the data from keys.js. 
+//requires the twitter npm package
 var twitter = require('twitter');
-var action = process.argv[2];
-var userName = process.argv[3];
-if(userName === 'undefined') {userName = 'RichardDotchin'};
-var params = {screen_name: userName};
-console.log(params);
+//requires the request npm package 
+var request = require('request');
+//link the keys.js file to liri
 var twitterKeysFile = require('./keys.js');
-//6. Then store the keys in a variable.
+
+//store the keys from keys.js in a variable.
 var twitterKeys = twitterKeysFile.twitterKeys;
 
 var client = new twitter(twitterKeys);
 
-
+//variables set to the action the user request and what they want to search
+var action = process.argv[2];
+//function that pulls recent tweets when action is set to my-tweets
 function twitterFunc() {	
+var userName = process.argv[3] || 'RichardDotchin';
+
+var params = {screen_name: userName};
 client.get('statuses/user_timeline', params, function(error, tweets, response){
 	if (error) return console.log(error);
-	console.log("Twitter user: " + params.screen_name + '\n')
+	console.log("Twitter user: @" + params.screen_name + '\n')
 	for(var i = 0; i < tweets.length; i++){
 		console.log('Date: ' + tweets[i].created_at + '\nTweeted: ' + tweets[i].text + '\n');
 	}
 	})
 }
 
+//function to be called when the user sets the variable action to movie-this
+function omdbFunc(){
+	//movie the user searches after 'movie-this' in process.argv[2]
+	var movie = process.argv[3] || 'Mr Nobody';
+
+	//omdb url to be used in the request
+	var url = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&tomatoes=true&r=json";
+	
+	//request version of an ajax call
+	request(url, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	  	//taking the response and changing from a string to an object
+	    body = JSON.parse(body); 
+
+	    //print the following information to the terminal
+	    console.log('Title: ' + body.Title + '\n' +
+	    			'Year: ' + body.Year + '\n' +
+	    			'IMDB Rating: ' + body.imdbRating + '\n' +
+	    			'Country: ' + body.Country + '\n' +
+	    			'Language: ' + body.Language + '\n' +
+	    			'Plot: ' + body.Plot + '\n' +
+	    			'Actors: ' + body.Actors + '\n'+
+	    			'Rotten Tomatoes Rating: ' + body.tomatoUserMeter + '\n' +
+	    			'Rotten Tomatoes URL: ' + body.tomatoURL);
+	  }
+	})
+}
+
+//switch statement calling the function related to the users action request
 switch(action){
 	case 'my-tweets':
 	twitterFunc();
+	break
+	case 'movie-this':
+	omdbFunc();
 	break
 }
--- spotify.js
+++ spotify.js
@@ -1,9 +1,11 @@
 var spotify = require('spotify');
 var action = process.argv[2];
-var song = process.argv[3];
-if(song === 'undefined') {song = 'never gonna give you up'};
+
+
 
 function spotifyFunc(){
+var song = process.argv[3] || 'never gonna give you up'; 
+
 spotify.search({type: 'track', query: 'The Sign'}, function(err, json){
 	if(err) {console.log(err)}
 		console.log(json.tracks.items[0]);
