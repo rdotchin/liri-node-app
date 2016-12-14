44b03aa500c25915c1e99b15278dc1c6faf449e0
Richard Dotchin <richard.dotchin@gmail.com>
2016-12-13 19:09:15 -0500

    comments and results styling'

--- liri.js
+++ liri.js
@@ -1,22 +1,29 @@
-//6. write the code you need to grab the data from keys.js. 
+/*VARIABLES & REQUIRE*/
+/*-----------------------------------------------------------------------------------*/
 //requires the twitter npm package
 var twitter = require('twitter');
 //requires the request npm package 
 var request = require('request');
 //require the spotify npm package
 var spotify = require('spotify');
-//require the fs npm package
+//require fs from node
 var fs = require('fs');
+//require child_process from node 
+var exec = require('child_process').exec;
 //link the keys.js file to liri
 var twitterKeysFile = require('./keys.js');
-
 //store the keys from keys.js in a variable.
 var twitterKeys = twitterKeysFile.twitterKeys;
 
 var client = new twitter(twitterKeys);
-
 //variables set to the action the user request and what they want to search
 var action = process.argv[2];
+
+/*END OF VARIABLES & REQUIRE*/
+/*-----------------------------------------------------------------------------------*/
+
+/*FUNCTIONS*/
+/*-----------------------------------------------------------------------------------*/
 //function that pulls recent tweets when action is set to my-tweets
 function twitterFunc() {	
 	//sets twitter user name to process.argv[3] or RichardDotchin if nothing entered
@@ -34,7 +41,7 @@ function twitterFunc() {
 			if(i == 20) {break}
 		}
 	});
-}
+} //end of twitterFunc()
 
 //function to be called when the user sets the variable action to movie-this
 function omdbFunc(){
@@ -51,7 +58,8 @@ function omdbFunc(){
 	    body = JSON.parse(body); 
 
 	    //print the following information to the terminal
-	    console.log('Title: ' + body.Title + '\n' +
+	    console.log( '--------OMDB RESULTS--------\n' + 
+	    			'Title: ' + body.Title + '\n' +
 	    			'Year: ' + body.Year + '\n' +
 	    			'IMDB Rating: ' + body.imdbRating + '\n' +
 	    			'Country: ' + body.Country + '\n' +
@@ -59,10 +67,11 @@ function omdbFunc(){
 	    			'Plot: ' + body.Plot + '\n' +
 	    			'Actors: ' + body.Actors + '\n'+
 	    			'Rotten Tomatoes Rating: ' + body.tomatoUserMeter + '\n' +
-	    			'Rotten Tomatoes URL: ' + body.tomatoURL);
+	    			'Rotten Tomatoes URL: ' + body.tomatoURL + 
+	    			'\n------END OF OMDB RESULTS------');
 	  }
 	});
-}
+} //end of omdbFunc()
 
 function spotifyFunc(){
 	//sets song to process.argv[3] or a default song if nothing entered
@@ -73,42 +82,52 @@ function spotifyFunc(){
 			
 			/*console.log(data.tracks.items[0]);*/
 			//console.log info for the song searched to the terminal
-			console.log('Artist: ' + data.tracks.items[0].artists[0].name + '\n' +
+			console.log('--------SPOTIFY RESULTS--------\n' + 
+						'Artist: ' + data.tracks.items[0].artists[0].name + '\n' +
 						'Song: ' + data.tracks.items[0].name + '\n' +
-						'Spotify URL: ' + data.tracks.items[0].external_urls.spotify);
+						'Spotify URL: ' + data.tracks.items[0].external_urls.spotify + 
+						'\n-------------------------------');
 	});
+} //end of spotifyFunc()
 
-}
-
-var exec = require('child_process').exec;
-
-
+//read random.txt file using fs and execute the command using child_process
 function fsFunc(){
 	fs.readFile('./random.txt', 'utf8', function(err, data) {
 		if(err) {console.log(err)}
-			
+			//command to run using the data read from random.txt
 			var cmd = 'node liri.js ' + data;
-
+			//execute the command in the variable cmd
 			exec(cmd, function(error, stdout, stderr) {
+				//console.log the results from the command
 				console.log(stdout);
 			})
-		
+	});
+} //end of fsFunc()
 
-	})
-}
+/*END OF FUNCTIONS*/
+/*-----------------------------------------------------------------------------------*/
 
+/*SWITCH STATEMENT*/
+/*-----------------------------------------------------------------------------------*/
 //switch statement calling the function related to the users action request
 switch(action){
+	//twitter
 	case 'my-tweets':
 	twitterFunc();
 	break
+	//omdb
 	case 'movie-this':
 	omdbFunc();
 	break
+	//spotify
 	case 'spotify-this-song':
 	spotifyFunc();
 	break
+	//read file and execute command
 	case 'do-what-it-says':
 	fsFunc();
 	break
-}
+} //end of switch(action) statement
+
+/*END OF SWITCH STATEMENT*/
+/*-----------------------------------------------------------------------------------*/
